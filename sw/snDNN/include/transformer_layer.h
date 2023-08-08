f16f49e96b52b28705663a8e8b249e1b7d260207
Viviane Potocnik <vivianep@iis.ee.ethz.ch>
2023-08-08 14:35:32 +0200

    snDNN: add function definition for transformer

---
+++ sw/snDNN/include/transformer_layer.h
@@ -0,0 +1,16 @@
+// Copyright 2020 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Viviane Potocnik <vivianep@iis.ee.ethz.ch>
+
+#include "layer.h"
+
+/**
+ * @brief  Transformer layer
+ *
+ * @param l transformer_layer struct that holds addresses and parameters
+ *
+ */
+
+void transformer_layer(transformer_layer_t* const l);
