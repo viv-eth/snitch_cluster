979ff4c5c0034f288b38807efe73910f604c9811
Viviane Potocnik <vivianep@iis.ee.ethz.ch>
2023-08-17 22:30:11 +0200

    snDNN: add new SoftMax implementation

--- sw/snDNN/src/softmax.h
+++ sw/snDNN/src/softmax.h
@@ -9,6 +9,15 @@
 // #include "printf.h"
 #include "utils.h"
 
+static inline float naive_exponential(float x) {
+    float result = 1.0f;
+    float term = 1.0f;
+    for (int i = 1; i < 10; i++) {
+        term *= x / i;
+        result += term;
+    }
+    return result;
+}
 /**
  * Implementation of the SoftMax layer.
  */
@@ -18,6 +27,7 @@ static inline void softmax_fp32(float *input, float *output, int32_t ldI,
                                 int32_t seq_len, int32_t input_samples) {
     float max_core = 0.0;  // max value of the current core
     float sum = 0.0;       // sum of the exp values of the current core
+    float exponent = 0.0;  // exponent of the current value
 
     // uint32_t compute_id = snrt_global_core_idx();
     // uint32_t num_cores = snrt_cluster_compute_core_num();
@@ -31,17 +41,27 @@ static inline void softmax_fp32(float *input, float *output, int32_t ldI,
                 if (input[b * batch_offset + s * ldI + i] > max_core) {
                     max_core = input[b * batch_offset + s * ldI + i];
                 }
+                // print the input values
+                // printf("input[%d] = %f\n", compute_id * input_samples + b * batch_offset + s * ldI + i,
+                //        input[b * batch_offset + s * ldI + i]);
             }
 
             // compute the shifted value of the current row
             for (int32_t i = 0; i < input_samples; i++) {
+                asm volatile(
+                    "fsub.s   %[exponent], %[input], %[max_core]\n\t"
+                    : [exponent] "+f"(exponent)
+                    : [input] "f"(input[b * batch_offset + s * ldI + i]),
+                      [max_core] "f"(max_core)
+                );
                 output[b * batch_offset + s * ldI + i] =
                     // FIXME: Below code is erroring due to the standard math
                     // lib conflict
                     // TODO: Try out with musl lib
                     // expf(input[b * batch_offset + s * ldI + i] - max_core);
-                    // FIXME: actually there should be an exponentiation
-                    input[b * batch_offset + s * ldI + i] - max_core;
+                    expf(exponent);
+                    // naive_exponential(exponent);
+                    // printf("exponent of %f = %f\n", exponent, expf(exponent));
                 sum += output[b * batch_offset + s * ldI + i];
             }
 
@@ -50,6 +70,11 @@ static inline void softmax_fp32(float *input, float *output, int32_t ldI,
                 // INFO: DIVSQRT unit MUST be activated in the cluster
                 // configuration
                 output[b * batch_offset + s * ldI + i] /= sum;
+                // asm volatile(
+                //     "fadd.s  ft8, %[output], %[zero]\n\t"
+                //     : [output] "+f"(output[b * batch_offset + s * ldI + i])
+                //     : [zero] "f"(0.0f)
+                // );
                 // printf("output[%d] = %f\n", compute_id * input_samples + b *
                 // batch_offset + s * ldI + i,
                 //        output[b * batch_offset + s * ldI + i]);
@@ -57,5 +82,5 @@ static inline void softmax_fp32(float *input, float *output, int32_t ldI,
         }
     }
 
-    snrt_cluster_hw_barrier();
+    // snrt_cluster_hw_barrier();
 }
