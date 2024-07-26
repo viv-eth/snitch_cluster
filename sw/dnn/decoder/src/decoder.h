672633806aee88642ba1b9f03f8a9087e72c482d
Viviane Potocnik <vivianep@iis.ee.ethz.ch>
2024-07-26 14:47:43 +0200

    [DECODER] Add multi-layer support

--- sw/dnn/decoder/src/decoder.h
+++ sw/dnn/decoder/src/decoder.h
@@ -14,6 +14,8 @@ typedef struct {
 } decoder_args_t;
 
 static inline void decoder_block(decoder_args_t *decoder_args) {
-    mha_layer(decoder_args->mha_args);
+    for (uint32_t i = 0; i < decoder_args->num_layers; i++) {
+        mha_layer(decoder_args->mha_args);
+    }
     mlp_layer(decoder_args->mlp_args);
 }
